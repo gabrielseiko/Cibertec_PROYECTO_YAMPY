7ce7b487037898902824cff2b743f339f5052c6f
seikosilvakomesu <seikosilvakomesu@gmail.com>
2023-09-25 19:34:49 -0500

    Cree el activity principal

---
+++ app/src/main/java/com/example/comcibertecyampy/PantallaInicio.kt
@@ -0,0 +1,11 @@
+package com.example.comcibertecyampy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PantallaInicio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla_inicio)
+    }
+}
